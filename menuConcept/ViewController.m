bfd97ee9f010234d2e57bebb2e4a318d445dada5
Leonard Schuetz <leni.schuetz13@gmail.com>
2014-03-09 01:27:05 +0100

    Fixed a bug

--- menuConcept/ViewController.m
+++ menuConcept/ViewController.m
@@ -20,10 +20,6 @@
     mainMenuView.delegate = self; //Important if you want to use delegate methods like MVDcurrentIndexWasUpdated:
     [self.view addSubview:mainMenuView];
     mainMenuView.drawButtonBorder = YES;
-    
-    
-    UIButton *yourButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
-    mainMenuView.button1_rect = yourButton.frame;
 }
 
 -(void)MVDcurrentIndexWasUpdated:(long int)currentIndex {
